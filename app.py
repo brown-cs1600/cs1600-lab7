b631c3aab6a2738795152c65944e523eb5d22266
Ian Rackow <ian.rackow@gmail.com>
2022-10-30 14:27:15 -0400

    App init

---
+++ app.py
@@ -0,0 +1,54 @@
+import threading
+import atexit
+from flask import Flask
+import random
+
+COLORS = ['RED', 'BLUE', 'GREEN']
+
+# variables that are accessible from anywhere
+color = random.choice(COLORS)
+
+# lock to control access to variable
+dataLock = threading.Lock()
+# thread handler
+colorThread = None
+
+def create_app():
+    app = Flask(__name__)
+
+    def interrupt():
+        global colorThread
+        colorThread.cancel()
+
+    def setColor():
+        global color
+        global colorThread
+        with dataLock:
+            color = random.choice(COLORS)
+
+        # Set the next thread to happen
+        colorThread = threading.Timer(random.uniform(0.5, 2.5), setColor, ())
+        colorThread.start()
+
+    def startThread():
+        # Do initialisation stuff here
+        global colorThread
+        # Create your thread
+        colorThread = threading.Timer(random.uniform(0.5, 2.5), setColor, ())
+        colorThread.start()
+
+    @app.route('/color')
+    def success():
+        with dataLock:
+            return color
+
+    # Initiate
+    startThread()
+    # When you kill Flask (SIGTERM), clear the trigger for the next thread
+    atexit.register(interrupt)
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
